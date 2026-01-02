17ed7654af7386a834c93c4e780693a59dbc4f04
kanaklatwal <kanaklatwal@gmail.com>
2026-01-02 09:57:01 +0530

    Fix CORS for Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,16 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://streamverse-eta-sandy.vercel.app"
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
